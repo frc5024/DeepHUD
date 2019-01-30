82881fe6a715f414a0fb077905a95197e393aac8
Evan Pratten <ewpratten@gmail.com>
2019-01-30 18:32:22 -0500

    working on js

---
+++ web/images.js
@@ -0,0 +1,25 @@
+// var visionCanvas = document.getElementById('vision').getContext("2d");
+// var driverCanvas = document.getElementById('driver').getContext("2d");
+// console.log("Loaded canvases");
+	
+// var visionImg = new Image();
+// var driverImg = new Image();
+// console.log("Created image objects");
+
+// visionImg.src = "http://10.50.24.2:1182/stream.mjpg";
+// driverImg.src = "http://10.50.24.2:1181/stream.mjpg";
+
+// visionImg.onload = function () {
+// 	console.log("Loaded vision");
+//   visionCanvas.drawImage(visionImg, 0, 0);
+// }
+
+// driverImg.onload = function () {
+// 	console.log("Loaded driver");
+//   driverCanvas.drawImage(driverImg, 0, 0);
+// }
+
+// console.log("Configured canvases");
+
+// document.getElementById('vision').src = "http://10.50.24.2:1182/stream.mjpg";
+// document.getElementById('driver').src = "http://10.50.24.2:1181/stream.mjpg";
