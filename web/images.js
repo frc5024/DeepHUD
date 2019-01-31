41ed212d2724f592f6880decf0729b787d5c907b
Evan Pratten <ewpratten@gmail.com>
2019-01-30 19:25:34 -0500

    added refresh button

--- web/images.js
+++ web/images.js
@@ -9,6 +9,12 @@
 // visionImg.src = "http://10.50.24.2:1182/stream.mjpg";
 // driverImg.src = "http://10.50.24.2:1181/stream.mjpg";
 
+// var width = window.innerWidth / 2;
+// var height = 160;
+
+// console.log(width);
+// console.log(height);
+
 // visionImg.onload = function () {
 // 	console.log("Loaded vision");
 //   visionCanvas.drawImage(visionImg, 0, 0);
@@ -19,7 +25,33 @@
 //   driverCanvas.drawImage(driverImg, 0, 0);
 // }
 
+
 // console.log("Configured canvases");
 
-// document.getElementById('vision').src = "http://10.50.24.2:1182/stream.mjpg";
-// document.getElementById('driver').src = "http://10.50.24.2:1181/stream.mjpg";
+// function updateImages(){
+// 	driverCanvas.drawImage(driverImg, 0, 0);
+// 	visionCanvas.drawImage(visionImg, 0, 0);
+// 	driverCanvas.width = width;
+// 	driverCanvas.height = height;
+// 	visionCanvas.width = width;
+// 	visionCanvas.height = height;
+// }
+
+function visionError(){
+	visionImg.src="./invalid.png";
+	console.log("Failed to load vision camera");
+}
+
+function driverError(){
+	driverImg.src="./invalid.png";
+	console.log("Failed to load driver camera");
+}
+
+var driverImg = document.getElementById("driver");
+var visionImg = document.getElementById("vision");
+
+driverImg.onerror = driverError;
+visionImg.onerror = visionError;
+
+// // document.getElementById('vision').src = "http://10.50.24.2:1182/stream.mjpg";
+// // document.getElementById('vision').src = "http://10.50.24.2:1181/stream.mjpg";
